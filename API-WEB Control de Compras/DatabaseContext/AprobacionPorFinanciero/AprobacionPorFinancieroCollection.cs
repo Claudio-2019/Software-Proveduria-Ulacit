ec60b2939b1dbb83745993350412af4a9527be84
Emanuel Hasbun <ehasbung@gmail.com>
2020-10-30 18:52:59 -0600

    aprobaciones por financiero

---
+++ API-WEB Control de Compras/DatabaseContext/AprobacionPorFinanciero/AprobacionPorFinancieroCollection.cs
@@ -0,0 +1,57 @@
+﻿using API_WEB_Control_de_Compras.DatabaseContext.Login;
+using API_WEB_Control_de_Compras.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_WEB_Control_de_Compras.DatabaseContext.AprobacionPorFinanciero
+{
+    public class AprobacionPorFinancieroCollection
+    {
+            internal DatabaseConfig repositorio = new DatabaseConfig();
+
+            private IMongoCollection<AprobacionPorFinancieroModel> Collection;
+
+            public AprobacionPorFinancieroCollection()
+            {
+                Collection = repositorio.database.GetCollection<AprobacionPorFinancieroModel>("AprobacionPorFinanciero");
+            }
+
+            public async Task<List<AprobacionPorFinancieroModel>> getAllAprobacionesPorFinanciero()
+            {
+                return await Collection.FindAsync(new BsonDocument()).Result.ToListAsync();
+            }
+
+            public async Task<AprobacionPorFinancieroModel> getAprobacionesPorFinancieroWithId(string id)
+            {
+
+                return await Collection.FindAsync(new BsonDocument { { "_id", new ObjectId(id) } }).Result.FirstAsync();
+            }
+
+            public async Task InsertarAprobacionPorFinanciero(AprobacionPorFinancieroModel aprobacion)
+            {
+                await Collection.InsertOneAsync(aprobacion);
+            }
+
+            public async Task UpdateAprobacionPorFinanciero(AprobacionPorFinancieroModel aprobacion)
+            {
+                var FiltroId = Builders<AprobacionPorFinancieroModel>.Filter.Eq(dato => dato._id, aprobacion._id);
+
+                await Collection.ReplaceOneAsync(FiltroId, aprobacion);
+            }
+
+            public async Task DeleteAprobacionPorFinanciero(string id)
+            {
+                var FiltroConsulta = Builders<AprobacionPorFinancieroModel>.Filter.Eq(s => s._id, new ObjectId(id));
+
+                await Collection.DeleteOneAsync(FiltroConsulta);
+
+            }
+        }
+    }
+
+}
+}
---
+++ API-WEB Control de Compras/DatabaseContext/AprobacionPorFinanciero/IAprobacionPorFinanciero.cs
@@ -0,0 +1,25 @@
+﻿using API_WEB_Control_de_Compras.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_WEB_Control_de_Compras.DatabaseContext.AprobacionPorFinanciero
+{
+    interface IAprobacionPorFinanciero
+    {
+
+namespace API_WEB_Control_de_Compras.DatabaseContext.Notificaciones
+    {
+        interface INotificaciones
+        {
+            Task InsertarAprobacionPorFinanciero(AprobacionPorFinancieroModel aprobacion);
+            Task UpdateAprobacionPorFinanciero(AprobacionPorFinancieroModel aprobacion);
+            Task DeleteAprobacionPorFinanciero(string id);
+            Task<AprobacionPorFinancieroModel> getAprobacionesPorFinancieroWithId(string id);
+            Task<List<AprobacionPorFinancieroModel>> getAllAprobacionesPorFinanciero();
+        }
+    }
+
+}
+}
