b3f04d7825e250ef65950bc14fe7ae72f8c47b16
daniel22r <72180607+daniel22r@users.noreply.github.com>
2020-11-01 18:22:59 -0600

    SolicituCompra
    Carpeta

---
+++ API-WEB Control de Compras/DatabaseContext/SolicitudCompra/ISolicitudCompra.cs
@@ -0,0 +1,17 @@
+﻿using API_WEB_Control_de_Compras.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_WEB_Control_de_Compras.DatabaseContext.SolicitudCompra
+{
+    interface ISolicitudCompra
+    {
+        Task InsertarSolicitudCompra(SolicitudCompraModel solicitudcompra);
+        Task UpdateSolicitudCompran(SolicitudCompraModel solicitudcompra);
+        Task DeleteSolicitudCompra(string id);
+        Task<SolicitudCompraModel> getSolicitudCompraWithId(string id);
+        Task<List<SolicitudCompraModel>> getAllSolicitudCompra();
+    }
+}
---
+++ API-WEB Control de Compras/DatabaseContext/SolicitudCompra/SolicitudCompraCollection.cs
@@ -0,0 +1,57 @@
+﻿using API_WEB_Control_de_Compras.DatabaseContext.Login;
+using API_WEB_Control_de_Compras.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_WEB_Control_de_Compras.DatabaseContext.SolicitudCompra
+{
+    public class SolicitudCompraCollection : ISolicitudCompraCollection
+    {
+        internal DatabaseConfig repositorio = new DatabaseConfig();
+
+        private IMongoCollection<SolicitudCompraModel> Collection;
+
+
+        public SolicutdCompraCollection()
+        {
+            Collection = repositorio.database.GetCollection<SolicitudCompraModel>("SolicitudCompra");
+
+        }
+
+        public async Task<List<SolicitudCompraModel>> getAllSolicitudCompra()
+        {
+            return await Collection.FindAsync(new BsonDocument()).Result.ToListAsync();
+        }
+
+        public async Task<SolicitudCompraModel> getSolicitudCompraWithId(string id)
+        {
+
+            return await Collection.FindAsync(new BsonDocument { { "_id", new ObjectId(id) } }).Result.FirstAsync();
+        }
+
+        public async Task InsertarSolicitudCompra(SolicitudCompraModel solicitudcompra)
+        {
+            await Collection.InsertOneAsync(solicitudcompra);
+        }
+
+        public async Task UpdateSolicitudCompra(SolicitudCompraModel solicitudcompra)
+        {
+            var FiltroId = Builders<SolicitudCompraModel>.Filter.Eq(dato => dato._id, solicitudcompra._id);
+
+            await Collection.ReplaceOneAsync(FiltroId, solicitudcompra);
+        }
+
+        public async Task DeleteSolicitudCompra(string id)
+        {
+            var FiltroConsulta = Builders<SolicitudCompraModel>.Filter.Eq(s => s._id, new ObjectId(id));
+
+            await Collection.DeleteOneAsync(FiltroConsulta);
+
+        }
+    }
+}
