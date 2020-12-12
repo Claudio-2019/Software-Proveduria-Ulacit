06dfca9ccc5c664421f776d4b1c873de0adee209
daniel22r <72180607+daniel22r@users.noreply.github.com>
2020-12-12 09:06:34 -0600

    apro recha solicitudes

--- Control-de-Compras/src/app/Components/solicitudes-financiero/solicitudes-financiero.component.ts
+++ Control-de-Compras/src/app/Components/solicitudes-financiero/solicitudes-financiero.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  Input, ɵɵqueryRefresh } from '@angular/core';
+import {ComprasModel} from 'src/app/Models/Compras'
+import {ComprasService} from 'src/app/Services/Compras/compras.service'
+import {UsuariosModel} from 'src/app/Models/Usuarios'
+import {AprobacionModel} from 'src/app/Models/Aprobacion'
+import {LoginServiceService} from 'src/app/Services/Login/login-service.service'
+
 
 @Component({
   selector: 'app-solicitudes-financiero',
@@ -7,12 +13,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitudesFinancieroComponent implements OnInit {
 
-  
-  
-  constructor() { }
+
+  solicitudes: ComprasModel[]
+  usuarios: UsuariosModel[]
+
+  constructor( private comprasAPI: ComprasService, private usuariosAPI: LoginServiceService) { }
+
+
+  cargaSolicitudes(){
+    this.comprasAPI.ComprasActuales().subscribe(resul =>{
+      this.solicitudes = resul
+    })
+  }
+
+  borrarCompras(compra: ComprasModel){
+    this.comprasAPI.BorrarCompra(compra).subscribe(val=>{
+        console.log(val)
+        
+    })
+  }
+
+  aprobarSolic(compra){
+    let aprob = this.refactorAprob(compra)
+    
+    this.comprasAPI.InsertAprobacion(aprob).subscribe(val=>{
+      console.log(val)
+    })
+
+  }
+
+  refactorAprob(compra){
+    let compraMod = compra
+    delete compraMod.tipoArticulo
+    delete compraMod._id
+    compraMod['estadoActual'] = true
+    //console.log(compra)
+    let aprobacion = compraMod
+    
+    //console.log(aprobacion)
+    return aprobacion
+  }
 
   ngOnInit() {
-    
+    this.cargaSolicitudes()
+    //this.cargaUsuarios()
   }
 
 }
+
+
+
+
+
